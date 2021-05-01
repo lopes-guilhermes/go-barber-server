5e78192f1108a27b00a32afd8495014da4eb566f
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-01 10:31:25 -0300

    estrutura inicial do projeto

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+  
+    const parsedDate = parseISO(date);
+    
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+  
+    const appointment = createAppointment.execute({provider, date: parsedDate});
+    
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
