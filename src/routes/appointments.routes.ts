3237fb42aa0185c9e4dd7de2a1da07bcbdcd01ba
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-05 14:02:15 -0300

    middleware autenticação de rotas

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,12 +4,17 @@ import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
+
+  console.log(request.user)
 
   return response.json(appointments);
 });
