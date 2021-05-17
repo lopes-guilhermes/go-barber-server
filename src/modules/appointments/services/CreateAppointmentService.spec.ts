40ea99296b1fbaa5c69ae33d6b89bc3b033d011f
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-16 21:52:21 -0300

    primeiro test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,21 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1234567890'
+    })
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234567890');
+  });
+  
+  // it('should be able to create two appointment on the same date', () => {
+
+  // });
+});
