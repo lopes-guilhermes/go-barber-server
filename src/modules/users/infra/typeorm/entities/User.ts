62ef43bdf1e56927f84260e6b6da058d99f0840b
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-06-03 13:44:41 -0300

    storage aws s3

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import uploadConfig from '@config/upload';
 
 import { Exclude, Expose } from 'class-transformer';
 
@@ -28,7 +29,17 @@ class User {
 
   @Expose({ name: 'avatar_url'})
   getAvatarUrl(): string | null {
-    return this.avatar ? `${process.env.APP_API_URL}/files/${this.avatar}` : null
+    if (!this.avatar)
+      return null;
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3-${process.env.AWS_REGION}.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
